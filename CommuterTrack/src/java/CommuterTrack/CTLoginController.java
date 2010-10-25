a0eebfd919fdfbe313d08ab46f6fc11d75a1e32e
dan.mechanic <dan.mechanic@d65b069b-54f7-7af8-298b-5c2af08a920c>
2010-10-25 00:56:55 +0000

    added 'method' to logincontroller, method=login|logout...  I failed to eliminated caching tho... and the back button works , we need to discuss how to fix that.

--- CommuterTrack/src/java/CommuterTrack/CTLoginController.java
+++ CommuterTrack/src/java/CommuterTrack/CTLoginController.java
@@ -38,11 +38,19 @@ public class CTLoginController extends HttpServlet {
     throws ServletException, IOException {
         String username;
         String password;
+        String method; // the method 'login' or 'logout' that we are running
         CTSessionRemote session;
         final Context context;
 
         username = request.getParameter("user");
         password = request.getParameter("pass");
+        method = request.getParameter("method");
+
+        // prevent caching
+        response.addHeader("Pragma","no-cache");
+        response.addIntHeader("Expires",-1);
+        response.addHeader("Cache-Control","no-cache, must-revalidate");
+
 
         try {
             context = new InitialContext();
@@ -53,28 +61,53 @@ public class CTLoginController extends HttpServlet {
             rd.forward(request, response);
             session = null;
         }
+            //hsn.setAttribute("user", "none");
             /* Create an HTTP session
              * Store the username so we can include it in the addRoute stuff
              */
-            HttpSession hsn = request.getSession();
-            hsn.setAttribute("user", "none");
+            HttpSession hsn = null;// = request.getSession();
+            //hsn.setAttribute("user", "none");
 
-        // Forward the request to the appropriate view
-        if (session.logInUser(username, password))
-        {
-            hsn.setAttribute("user", username);
 
-            //System.out.println("user logged in");
-            RequestDispatcher rd  = request.getRequestDispatcher("timer.jsp");
-            rd.forward(request, response);
+        if (method.equals("login")) {
+            hsn = request.getSession();
+            //hsn.setAttribute("user", "none");
+            // try to login
+            // Forward the request to the appropriate view
+            if (session.logInUser(username, password)) {
+                hsn.setAttribute("user", username);
+
+                //System.out.println("user logged in");
+
+                RequestDispatcher rd = request.getRequestDispatcher("timer.jsp");
+                rd.forward(request, response);
+            } else {
+                hsn.invalidate();
+                //System.out.println("user could not log in");
+                RequestDispatcher rd = request.getRequestDispatcher("login_fail.jsp");
+                rd.forward(request, response);
+            }
+        } else if (method.equals("logout")) {
+                Logger.getLogger(CTLoginController.class.getName()).log(Level.WARNING,"REACHED LOGOUT","REACHED LOGOUT");
+
+            try {
+                hsn = request.getSession();
+                hsn.setAttribute("user", "none");
+                hsn.invalidate();
+
+            } catch (Exception e) {
+                Logger.getLogger(CTLoginController.class.getName()).log(Level.SEVERE, e.toString(), e.toString());
+
+
+            }
+
+        
+
+        RequestDispatcher rd = request.getRequestDispatcher("index.jsp");
+        rd.forward(request, response);
+
         }
-        else
-        {
-            //System.out.println("user could not log in");
-            RequestDispatcher rd = request.getRequestDispatcher("login_fail.jsp");
-            rd.forward(request, response);
-        }
-    } 
+    }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /** 
--- CommuterTrack/web/timer.jsp
+++ CommuterTrack/web/timer.jsp
@@ -4,16 +4,34 @@
     Author     : dm2474
 --%>
 
-<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<%@page contentType="text/html" pageEncoding="UTF-8"
+        import="CommuterTrack.*" %>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
    "http://www.w3.org/TR/html4/loose.dtd">
 
 <html>
     <head>
+        <meta http-equiv="Pragma" content="no-cache">
+        <meta http-equiv="expires" content=-1>
+        <meta http-equiv="no-cache"
+              <%
+        response.setHeader("Cache-Control","no-cache"); //Forces caches to obtain a new copy of the page from the origin server
+        response.setHeader("Cache-Control","no-store"); //Directs caches not to store the page under any circumstance
+        response.setDateHeader("Expires", 0); //Causes the proxy cache to see the page as "stale"
+        response.setHeader("Pragma","no-cache"); //HTTP 1.0 backward compatibility
+        String userName = (String) session.getAttribute("user");
+        if (null == userName) {
+             request.setAttribute("Error", "Session has ended.  Please login.");
+             RequestDispatcher rd = request.getRequestDispatcher("login_fail.jsp");
+             rd.forward(request, response);
+        }
+%>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <title>JSP Page</title>
     </head>
     <body>
         <h3>Placeholder for the timer page</h3>
+        Hello <%= ((String)session.getAttribute("user"))%>!
+        <a href="/CommuterTrack/CTLoginController?method=logout">logout</a>
     </body>
 </html>
